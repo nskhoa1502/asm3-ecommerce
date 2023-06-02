5b782f28a58b9908dc41b846151ebdbb8478d6a6
nskhoa1502 <nskhoa1502@gmail.com>
2023-06-02 08:48:10 +0700

    Sua cors lan 3

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,8 +70,8 @@ exports.postLogin = async (req, res, next) => {
     res
       .cookie("access_token", accessToken, {
         httpOnly: true,
-        // sameSite: "none",
-        secure: true,
+        sameSite: "lax",
+        // secure: true,
         // other options
       })
       .status(200)
